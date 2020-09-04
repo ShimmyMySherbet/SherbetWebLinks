d9b7b09a10f6a9b2f6a6543ea31cce4135ae0a21
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2020-09-04 16:13:35 +1000

    Add project files.

---
+++ FuckSherbetWebLinks/extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SherbetWebLinks
+{
+    public static class extensions
+    {
+        public static string Translate(this string Key, params object[] Parameters)
+        {
+            return SherbetWebLinks.Instance.Translate(Key, Parameters);
+        }
+    }
+}
---
+++ SherbetWebLinks/Models/WeblinksConfig.cs
@@ -0,0 +1,18 @@
+﻿using Rocket.API;
+
+namespace SherbetWebLinks.Models
+{
+    public class WeblinksConfig : IRocketPluginConfiguration
+    {
+        public string DiscordInvite;
+        public string WebsiteURL;
+        public string ApplyStaffURL;
+
+        public void LoadDefaults()
+        {
+            DiscordInvite = "https://discord.gg/Kcx2dvE";
+            WebsiteURL = "https://YourWebsite.com/";
+            ApplyStaffURL = "https://YourWebsite.com/ApplyStaff";
+        }
+    }
+}
